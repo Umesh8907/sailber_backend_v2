8c48c4f6ade7f9852eec0cb50e994f6724b2771f
umesh8907 <umesh.tandon.51@gmail.com>
2025-06-14 16:23:31 +0530

    refactor: update Firebase initialization to use service account from environment variable or local file

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,22 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
-import serviceAccount from '../google-services.json' assert { type: 'json' };
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  const filePath = path.resolve(__dirname, '../firebase-service-account.json');
+  const jsonData = readFileSync(filePath, 'utf-8');
+  serviceAccount = JSON.parse(jsonData);
+}
 
 initializeApp({
   credential: cert(serviceAccount),
